c3b6a4ed770d9e9843f30f306cbb1ba2c1c86b98
raphael <raphael.moynat@gmail.com>
2025-04-08 17:32:18 +0200

    test cam

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,70 +1,101 @@
-import React, { useRef, useEffect, useState } from 'react';
-import jsQR from 'jsqr';
+import React, { useRef, useState, useEffect } from 'react'
+import jsQR from 'jsqr'
 
-const QRCodeScanner = () => {
-    const videoRef = useRef(null);
-    const canvasRef = useRef(null);
-    const [qrContent, setQRContent] = useState(null);
+function QRCodeScanner() {
+    const videoRef = useRef(null)
+    const canvasRef = useRef(null)
+    const [result, setResult] = useState("")
+    const [scanning, setScanning] = useState(false)
 
     useEffect(() => {
-        const startCamera = async () => {
+        async function startCamera() {
             try {
                 const stream = await navigator.mediaDevices.getUserMedia({
-                    video: {
-                        facingMode: 'environment',
-                        width: { ideal: 640 },
-                        height: { ideal: 480 },
-                        frameRate: { ideal: 15 },
-                    },
-                });
-                if (videoRef.current) {
-                    videoRef.current.srcObject = stream;
+                    video: { facingMode: "environment" }
+                })
+
+                videoRef.current.srcObject = stream
+                videoRef.current.onloadedmetadata = () => {
+                    videoRef.current.play()
+                    setScanning(true)
                 }
             } catch (error) {
-                console.error(error);
-            }
-        };
-
-        startCamera();
-
-        const interval = setInterval(() => {
-            scanQRCode();
-        }, 500);
-
-        return () => clearInterval(interval);
-    }, []);
-
-    const scanQRCode = () => {
-        if (canvasRef.current && videoRef.current) {
-            const canvas = canvasRef.current;
-            const context = canvas.getContext('2d');
-            if (context) {
-                context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-                const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-                const code = jsQR(imageData.data, imageData.width, imageData.height);
-                if (code) {
-                    setQRContent(code.data);
-                    console.log(code.data);
-                } else {
-                    setQRContent(null);
-                }
+                console.log(error)
             }
         }
-    };
+
+        startCamera()
+
+        return () => {
+            if (videoRef.current && videoRef.current.srcObject) {
+                videoRef.current.srcObject.getTracks().forEach(track => track.stop())
+            }
+        }
+    }, [])
+
+    useEffect(() => {
+        let frameId
+
+        function scanQR() {
+            if (!scanning) return
+
+            if (videoRef.current && canvasRef.current) {
+                const video = videoRef.current
+                const canvas = canvasRef.current
+
+                canvas.width = video.videoWidth
+                canvas.height = video.videoHeight
+
+                const ctx = canvas.getContext('2d')
+                ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+                const code = jsQR(imageData.data, imageData.width, imageData.height)
+                if (code) {
+                    setResult(code.data)
+                }
+            }
+
+            frameId = requestAnimationFrame(scanQR)
+        }
+
+        if (scanning) {
+            frameId = requestAnimationFrame(scanQR)
+        }
+
+        return () => {
+            if (frameId) {
+                cancelAnimationFrame(frameId)
+            }
+        }
+    }, [scanning])
+
 
     return (
         <div>
-            <div style={{ width: '320px', height: '240px' }}>
-                <video ref={videoRef} width="320" height="240" autoPlay />
-                <canvas ref={canvasRef} width="320" height="240" />
+            <div style={{ position: "relative" }}>
+                <video
+                    ref={videoRef}
+                    style={{ width: "100%", maxWidth: "400px" }}
+                    playsInline
+                />
+                <canvas
+                    ref={canvasRef}
+                    style={{ display: "none" }}
+                />
             </div>
-            {qrContent && (
-                <div>
-                    <p>{qrContent}</p>
-                </div>
-            )}
-        </div>
-    );
-};
 
-export default QRCodeScanner;
+            <div style={{ marginTop: "20px" }}>
+                <h3>QR Code Result:</h3>
+                {result ? (
+                    <p>{result}</p>
+                ) : (
+                    <p>QR code</p>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default QRCodeScanner
