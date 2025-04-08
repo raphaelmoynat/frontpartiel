3f59adb0785e0466eb22fc929f177f1df15d9ed8
raphael <raphael.moynat@gmail.com>
2025-04-08 17:20:41 +0200

    test cam

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,44 +2,51 @@ import React, { useRef, useEffect, useState } from 'react';
 import jsQR from 'jsqr';
 
 const QRCodeScanner = () => {
-    const videoRef = useRef(null)
-    const canvasRef = useRef(null)
-    const [qrContent, setQRContent] = useState(null)
+    const videoRef = useRef(null);
+    const canvasRef = useRef(null);
+    const [qrContent, setQRContent] = useState(null);
 
     useEffect(() => {
         const startCamera = async () => {
             try {
-                const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    video: {
+                        facingMode: 'environment',
+                        width: { ideal: 640 },
+                        height: { ideal: 480 },
+                        frameRate: { ideal: 15 },
+                    },
+                });
                 if (videoRef.current) {
-                    videoRef.current.srcObject = stream
+                    videoRef.current.srcObject = stream;
                 }
             } catch (error) {
-                console.error(error)
+                console.error(error);
             }
         };
 
-        startCamera()
+        startCamera();
 
         const interval = setInterval(() => {
-            scanQRCode()
-        }, 100)
+            scanQRCode();
+        }, 500);
 
-        return () => clearInterval(interval)
-    }, [])
+        return () => clearInterval(interval);
+    }, []);
 
     const scanQRCode = () => {
         if (canvasRef.current && videoRef.current) {
             const canvas = canvasRef.current;
-            const context = canvas.getContext('2d')
+            const context = canvas.getContext('2d');
             if (context) {
                 context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
                 const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
                 const code = jsQR(imageData.data, imageData.width, imageData.height);
                 if (code) {
-                    setQRContent(code.data)
-                    console.log( code.data)
+                    setQRContent(code.data);
+                    console.log(code.data);
                 } else {
-                    setQRContent(null)
+                    setQRContent(null);
                 }
             }
         }
